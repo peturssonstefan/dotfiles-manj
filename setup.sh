b03c4d98eb7ab05e2aa766d14dc27f2177356c8b
peturssonstefan <petursson.stefan@gmail.com>
2022-02-21 12:52:32 +0100

    Installs

--- setup.sh
+++ setup.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
+chmod +x ./installations/setup_installations.sh
 chmod +x ./github/setup_github.sh
 chmod +x ./urxvt/setup_urxvt.sh
 
+cd ./installations/ && ./setup_installations.sh && cd ..
 cd ./github/ && ./setup_github.sh && cd ..
 cd ./urxvt/ && ./setup_urxvt.sh && cd ..
-
